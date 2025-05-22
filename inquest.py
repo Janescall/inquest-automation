677689638e1afd125d877ee6746ab4c6e6d71bd2
Janescall <160820874+Janescall@users.noreply.github.com>
2025-05-22 15:15:01 +0900

    Update inquest.py

--- inquest.py
+++ inquest.py
@@ -60,4 +60,5 @@ final_result = {
 }
 
 # 콘솔에 전체 구조 출력 (UTF-8)
-sys.stdout.buffer.write((json.dump(final_result, f, ensure_ascii=False, separators=(',', ':')) + "\n").encode("utf-8"))
+sys.stdout.buffer.write((json.dumps(final_result, ensure_ascii=False, separators=(',', ':')) + "\n").encode("utf-8"))
+
